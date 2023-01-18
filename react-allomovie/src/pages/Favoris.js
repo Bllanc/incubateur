031b6beeb603c714ce489e1e824fb752884ecea4
Allan Averlan <allan.averlan@laposte.net>
2023-01-18 15:51:54 +0100

    first commit

---
+++ react-allomovie/src/pages/Favoris.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Favoris = () => {
+    return (
+
+        <div>
+            <Navigation />
+            <h1>Favoris</h1>
+        </div>
+    );
+};
+
+export default Favoris;
